ce68de4c200b92e4cf0022d690aefcf644164cf0
Aymeric Le Moal <45513384+Salyel@users.noreply.github.com>
2020-04-09 14:59:45 +0200

    Modification du perceptron (ne marche pas tout le temps)

--- DUMOULIN_LEMOAL_OPTIM_CONTINUE/main.py
+++ DUMOULIN_LEMOAL_OPTIM_CONTINUE/main.py
@@ -7,6 +7,9 @@ import math
 import perceptron
 from perceptron import Perceptron
 
+from sklearn import datasets
+from sklearn.decomposition import PCA
+
 
 #donnees_brutes = pd.read_csv('data_buy_sell.csv', sep=";", index_col=0)
 
@@ -17,9 +20,33 @@ from perceptron import Perceptron
 #perceptron = Perceptron(donnees_brutes)
 #perceptron.imprimer()
 
-donnees = np.random.random((10,4))
-objectif = 0.3
-#print(donnees)
+donnees = [[2, 3, 2, 2], [60, 3, 7, 2], [40, 3, 2, 8], [40, 9, 2, 2], [2, 2, 2, 2]]
+objectif = [0, 1, 1, 1, 0]
+lambdda = 0.1
 
-perceptron = Perceptron(donnees, objectif)
-perceptron.calculFonctionActivation()
+donnees2 = [[2, 3, 2, 2], [60, 3, 7, 2], [20, 3, 2, 8], [40, 9, 2, 2], [20, 2, 2, 2], [2, 2, 2, 2], [40, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]]
+objectif2 = [0, 1, 1, 1, 0, 1, 0, 1, 0]
+
+iris = datasets.load_iris()
+X = iris.data[:100, :2]  # we only take the first two features.
+y = iris.target[:100]
+#print(X)
+#print(y)
+
+Xo = []
+for i in range (len(X)):
+    Xoo = []
+    for j in range (len(X[i])):
+        Xoo.append(X[i][j])
+    Xoo.append(1)
+    Xo.append(Xoo)
+
+for i in range (len(donnees)):
+    donnees[i].append(1)
+
+perceptron = Perceptron(4)
+for i in range (500):
+    print("// Itération", i)
+    perceptron.calculFonctionEntrainement(donnees, objectif, lambdda)
+
+
--- DUMOULIN_LEMOAL_OPTIM_CONTINUE/perceptron.py
+++ DUMOULIN_LEMOAL_OPTIM_CONTINUE/perceptron.py
@@ -9,31 +9,35 @@ from random import *
 class Perceptron:
     
     #Constructor
-    def __init__(self, donnees, objectif):
-        self.donnees = donnees
-        self.weights = np.random.random((4,1))
-        self.bias = random()*5
-        print(self.bias)
-        self.cout = 0
-        self.valeurs = np.zeros((len(donnees), 1))
-        self.objectif = objectif
+    def __init__(self, input_size): 
+        self.weights = np.random.random((input_size + 1, 1))
+       # self.weights = [1.2, 1, 1, 1, 1]
         
     def sigmoide(self, x):
         return 1 / (1 + math.exp(-x))
     
     def tangenteHyperbolique(self, x):
         return (1 / (1 + math.exp(-2*x))) + 1
-        
-    def calculFonctionActivation(self):
-        for i in range (len(self.donnees)):
-            self.valeurs[i] = self.sigmoide((np.dot(self.donnees[i], self.weights) + self.bias))
-        print(self.valeurs)
-        for i in range (len(self.valeurs)):
-            self.cout += (self.valeurs[i] - self.objectif)**2
-        print(self.cout)
-        if (self.cout > 4):
-            print('méchant algorithme nul')
-        else:
-            print('c bien')
-            
     
+    def miseAJourPoids(self, donnees, valeurs, lambdda, objectif):
+        for j in range (len(donnees)):
+            print("Début de l'entrainement sur la donnée", j)
+            sommeDonnees = np.sum(donnees[j]) 
+            print("La somme de la donnée est égale à", sommeDonnees)
+            print("valeur égale à", valeurs[j])
+            for i in range (len(self.weights)):
+                self.weights[i] = self.weights[i] - ((lambdda * (sommeDonnees + 1) * valeurs[j] * (1 - valeurs[j]) * (valeurs[j] - objectif[j])) * donnees[j][i]) 
+        
+    def calculFonctionEntrainement(self, donnees, objectif, lambdda):
+        valeurs = []
+        for i in range (len(donnees)):
+            #donnees[i].append(1)
+            valeurs.append(self.sigmoide((np.dot(donnees[i], self.weights))))
+        for i in range (len(self.weights)):
+            print(self.weights[i]) 
+        self.miseAJourPoids(donnees, valeurs, lambdda, objectif)
+
+                
+            
+            
+                
